7abfc74c1c6d54ebeccb9fcd9583ca6c8b5aee49
MariaLight <61028743+MariaLight@users.noreply.github.com>
2025-03-12 17:29:06 +1200

    make FieldLayout class component

--- src/components/Field/FieldLayout.jsx
+++ src/components/Field/FieldLayout.jsx
@@ -1,19 +1,24 @@
 import styles from './field.module.css';
-import { selectCurrentPlayer, selectField } from '../../selectors';
-import { useSelector } from 'react-redux'
+import {connect } from 'react-redux'
+import { Component } from 'react'
 
+export class FieldLayoutContainer extends Component {
+    constructor(props) {
+        super(props);
+    }
 
-export const FieldLayout = ({ makeMove }) => {
-
-    const currentPlayer = useSelector(selectCurrentPlayer);
-    const field = useSelector(selectField);
-
-
-    return (
-        <div className={styles.field}>
-            {field.map((item, index) =>
-                <button key={index} onClick={() => makeMove(currentPlayer, index)} className={`${styles.fieldButton} ${item && styles[item]} ${item && styles.disabled}`}>{item}</button>
-            )}
-        </div >
-    );
+    render() {
+        return (
+            <div className={styles.field}>
+                {this.props.field.map((item, index) =>
+                    <button key={index} onClick={() => this.props.makeMove(this.props.currentPlayer, index)} className={`${styles.fieldButton} ${item && styles[item]} ${item && styles.disabled}`}>{item}</button>
+                )}
+            </div >
+        )
+    }
 }
+const mapStateToProps = (state) => ({
+    currentPlayer: state.currentPlayer,
+    field: state.field
+})
+export const FieldLayout = connect(mapStateToProps)(FieldLayoutContainer);
