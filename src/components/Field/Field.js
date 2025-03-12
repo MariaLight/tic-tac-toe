8ea692ccb78a61608ea4f78478ab17c6fd65d77b
MariaLight <61028743+MariaLight@users.noreply.github.com>
2025-03-12 18:41:45 +1200

    make Field class component

--- src/components/Field/Field.js
+++ src/components/Field/Field.js
@@ -1,36 +1,45 @@
 import { FieldLayout } from "./FieldLayout";
-import { selectField, selectIsGameEnded } from "../../selectors";
-import { useDispatch, useSelector } from "react-redux";
+import { connect } from "react-redux";
 import { checkAllFieldsFilled, checkIfWinner } from '../../helperFunctions';
 import { setField, setIsGameEnded, setIsDraw, setCurrentPlayer } from "../../actions";
+import { Component } from 'react'
 
-export const Field = () => {
+export class FieldContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.makeMove = this.makeMove.bind(this)
 
-    const isGameEnded = useSelector(selectIsGameEnded);
-    const field = useSelector(selectField);
-    const dispatch = useDispatch();
-
-
-
-    const makeMove = (currentPlayer, index) => {
-        let currentField = [...field];
-        if (!currentField[index] && !isGameEnded) {
+    }
+    makeMove(currentPlayer, index) {
+        let currentField = [...this.props.field];
+        if (!currentField[index] && !this.props.isGameEnded) {
             currentField[index] = currentPlayer;
 
-            dispatch(setField(currentField))
+            this.props.onSetField(currentField)
             if (checkIfWinner(currentPlayer, currentField)) {
-                dispatch(setIsGameEnded(true))
+                this.props.onGameEnded();
             } else if (checkAllFieldsFilled(currentField)) {
-                dispatch(setIsGameEnded(true))
-                dispatch(setIsDraw(true))
+                this.props.onGameEnded();
+                this.props.onDraw();
             }
             else {
                 const newCurrentPlayer = currentPlayer === 'x' ? 'o' : 'x';
-                dispatch(setCurrentPlayer(newCurrentPlayer))
+                this.props.onCurrentPlayerChange(newCurrentPlayer)
             }
         }
 
     }
 
-    return <FieldLayout makeMove={makeMove} />;
+    render() { return <FieldLayout makeMove={this.makeMove} />; }
 }
+const mapStateToProps = (state) => ({
+    isGameEnded: state.isGameEnded,
+    field: state.field
+})
+const mapDispatchToProps = (dispatch) => ({
+    onGameEnded: () => dispatch(setIsGameEnded(true)),
+    onDraw: () => dispatch(setIsDraw(true)),
+    onCurrentPlayerChange: (newCurrentPlayer) => dispatch(setCurrentPlayer(newCurrentPlayer)),
+    onSetField: (currentField) => dispatch(setField(currentField))
+})
+export const Field = connect(mapStateToProps, mapDispatchToProps)(FieldContainer);
